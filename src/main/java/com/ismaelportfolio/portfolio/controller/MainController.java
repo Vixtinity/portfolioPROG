88ea11cda70642153569c477b7d3e870b40113eb
Ismael <08062006ismael@gmail.com>
2025-03-07 18:15:25 +0100

    Arreglos a las paginas, cosas esteticas y menu hecho bootstrap

--- src/main/java/com/ismaelportfolio/portfolio/controller/MainController.java
+++ src/main/java/com/ismaelportfolio/portfolio/controller/MainController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class MainController {
 
     @GetMapping("/index")
-    public String mostrarInicio() {
+    public String mostrarInicio() { 
         return "index";
     }
 

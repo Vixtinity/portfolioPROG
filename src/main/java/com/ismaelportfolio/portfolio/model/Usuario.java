08071b734755a1d83505fe3f512d24b13ebc0cac
Ismael <08062006ismael@gmail.com>
2025-03-07 23:36:40 +0100

    carga de datos estatica a index

---
+++ src/main/java/com/ismaelportfolio/portfolio/model/Usuario.java
@@ -0,0 +1,61 @@
+package com.ismaelportfolio.portfolio.model;
+
+public class Usuario {
+    private String nombre;
+    private int edad;
+    private String descripcion;
+    private String curriculum;
+    private String empresa;
+    private String urlEmpresa;
+
+    public Usuario() {
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getCurriculum() {
+        return curriculum;
+    }
+
+    public void setCurriculum(String curriculum) {
+        this.curriculum = curriculum;
+    }
+
+    public String getEmpresa() {
+        return empresa;
+    }
+
+    public void setEmpresa(String empresa) {
+        this.empresa = empresa;
+    }
+
+    public String getUrlEmpresa() {
+        return urlEmpresa;
+    }
+
+    public void setUrlEmpresa(String urlEmpresa) {
+        this.urlEmpresa = urlEmpresa;
+    }
+}
